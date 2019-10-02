f874229738b32439f8acee5256763f2899f62d94
Shuyoou <zane_you@foxmail.com>
2019-10-02 15:36:16 +0800

    feat: mergeTwoLists

---
+++ merge-two-lists/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+class ListNode {
+  val: Number;
+  next: ListNode;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+export const mergeTwoLists = (l1: ListNode, l2: ListNode) => {
+  if (l1 === null) {
+    return l2;
+  } else if (l2 === null) {
+    return l1;
+  } else if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
