afbcd13e02bd07a6c498fe0dbe1daf8cffa6403b
Shuyoou <zane_you@foxmail.com>
2020-08-27 18:34:21 +0800

    update 0332

--- src/0332.find-itinerary/findItinerary.ts
+++ src/0332.find-itinerary/findItinerary.ts
@@ -1,3 +1,25 @@
 function findItinerary(tickets: string[][]): string[] {
+  let start = 'JFK'
+  const ticketMap= new Map()
 
+  tickets.forEach((ticket) => {
+    const [from , to] = ticket
+    if (ticketMap.has(from)) {
+      const target = ticketMap.has(from)
+      if (to < target[1]) {
+        ticketMap.set(from, ticket)
+      }
+    } else {
+      ticketMap.set(from , ticket)
+    }
+  })
+
+  const result = [start]
+  for (let i = 0; i < tickets.length; i++) {
+    const [from, to] = ticketMap.get(start)
+    result.push(to)
+    start = to
+  }
+  console.info(result)
+  return []
 };
