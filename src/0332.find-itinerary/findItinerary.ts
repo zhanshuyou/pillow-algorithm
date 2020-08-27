05ebcb55e00089f421fe324f8e7d7c8061edce42
Shuyoou <zane_you@foxmail.com>
2020-08-27 23:15:23 +0800

    update 332. 重新安排行程

--- src/0332.find-itinerary/findItinerary.ts
+++ src/0332.find-itinerary/findItinerary.ts
@@ -1,25 +1,59 @@
 function findItinerary(tickets: string[][]): string[] {
-  let start = 'JFK'
-  const ticketMap= new Map()
-
-  tickets.forEach((ticket) => {
-    const [from , to] = ticket
-    if (ticketMap.has(from)) {
-      const target = ticketMap.has(from)
-      if (to < target[1]) {
-        ticketMap.set(from, ticket)
-      }
-    } else {
-      ticketMap.set(from , ticket)
-    }
-  })
-
-  const result = [start]
-  for (let i = 0; i < tickets.length; i++) {
-    const [from, to] = ticketMap.get(start)
-    result.push(to)
-    start = to
-  }
-  console.info(result)
-  return []
+  const start = 'JFK'
+  const plan = new Plan(tickets, start)
+  return plan.getPlan()
 };
+
+class Plan {
+  private result = ['JFK']
+  private ticketMap = new Map()
+  private tickets: string[][] = []
+
+  constructor(tickets: string[][], start: string) {
+    this.tickets = tickets
+    this.buildMap(tickets)
+    this.dfs(start, 0)
+  }
+
+  private buildMap(tickets: string[][]) {
+    tickets.forEach((ticket) => {
+      const [from , to] = ticket
+      if (this.ticketMap.has(from)) {
+        const target = this.ticketMap.get(from)
+        this.ticketMap.set(from, target.concat(to).sort((item1: string, item2: string) => item1 > item2 ? 1 : -1))
+      } else {
+        this.ticketMap.set(from , [to])
+      }
+    })
+  }
+
+  private dfs(city: string, usedTickets: number) {
+    if (usedTickets === this.tickets.length) {
+      return true
+    }
+
+    const nextCities = this.ticketMap.get(city)
+    if (!nextCities) {
+      return false
+    }
+
+    for (let i = 0; i < nextCities.length; i++) {
+      const nextCity = nextCities[i]
+      this.result.push(nextCity)
+      nextCities.splice(i, 1)
+
+      if (this.dfs(nextCity, usedTickets + 1)) {
+        return true
+      } else {
+        nextCities.splice(i, 0, nextCity)
+        this.result.pop()
+      }
+    }
+
+    return false
+  }
+
+  public getPlan() {
+    return this.result
+  }
+}
